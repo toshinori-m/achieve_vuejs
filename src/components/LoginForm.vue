f1228dbc928bea25d8a1132edc05db07b4cfc064
tosinori-m <your.email@example.com>
2022-07-02 17:17:48 +0900

    Add vue

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <h2>ログイン</h2>
+    <form @submit.prevent="login">
+      <input type="email" required placeholder="メールアドレス" v-model="email">
+      <input type="password" required placeholder="パスワード" v-model="password">
+      <div class="error">{{ error }}</div>
+      <button>ログインする</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  emits: ['redirectToChatRoom'],
+  data () {
+    return {
+      email: '',
+      password: '',
+      error: null
+    }
+  },
+  methods: {
+    async login() {
+      try {
+        this.error = null
+
+        const res = await axios.post('http://localhost:3000/auth/sign_in', {
+          email: this.email,
+          password: this.password,
+          }
+        )
+        if (!res) {
+          throw new Error('メールアドレスかパスワードが違います')
+        }
+        if (!this.error) {
+          this.$emit('redirectToChatRoom')
+        }
+        console.log({ res })
+
+        return res
+      } catch (error) {
+        console.log({ error })
+        this.error = 'メールアドレスかパスワードが違います'
+      }
+    }
+  }
+}
+</script>
---
+++ src/components/TestComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    これはコンポーネントから表示されています。
+    <p>{{ message }}</p>
+    <button @click="onClick">親メソッドを実行するボタン</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['message'],
+  methods: {
+    onClick() {
+      this.$emit('toggle')
+    }
+  }
+}
+</script>
