cfc78b0a94d0bab9626d04b2338cd772417b2c8a
tosinori-m <your.email@example.com>
2022-07-02 13:41:43 +0900

    Add vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Welcome from '../views/WelcomePage'
 
-
-const routes = []
+const routes = [
+  {
+    path: '/',
+    name: 'WelcomePage',
+    component: Welcome
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
---
+++ src/views/WelcomePage.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <p v-if="isEnabled">{{ subtitle }}</p>
+    <button @dblclick="toggle">トグルする</button>
+    <p>{{ text }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: '初めてのVue.jsアプリです！',
+      subtitle: 'ようこそ',
+      isEnabled: true
+    }
+  },
+  computed: {
+    text () {
+      if (this.isEnabled) {
+        return 'こんにちは！'
+      } else {
+        return 'さよなら！'
+      }
+    }
+  },
+  methods: {
+    toggle () {
+      this.isEnabled = !this.isEnabled
+    }
+  }
+}
+</script>
