372d54470f12a6f7c69ba61d62bacbc85b2d8f5b
tosinori-m <your.email@example.com>
2022-08-06 15:26:21 +0900

    Add report_page

--- src/views/ReportPage.vue
+++ src/views/ReportPage.vue
@@ -3,26 +3,24 @@
     <h2>今日の達成</h2>
     <form @submit.prevent ="report">
       <p>登録年月日</p>
-      <!-- <input reports.date_field: DatePicker_value, value: date> -->
-      <!-- <p>{{ DatePicker.value, value: date }}</p> -->
-      <!-- <p>{{ DatePicker.value }}</p> -->
-      <!-- <input date_field: ="aim_1" required placeholder="１〜３月" v-model="aim_1"> -->
-
-
-      <!-- <input :reports="datepicker_value"> -->
-      <input type="datepicker_value :value">
-      <!-- <input type="aim" required placeholder="今期目標を入力" v-model="aim"> -->
-
+      <p>{{ report_date }}</p>
       <div class="error">{{ error }}</div>
       <button class="ok_button">ok</button>
     </form>
   </div>
 </template>
-
 <script>
-
+  export default {
+    date () {
+      return {
+        report_date: "",
+      }
+    },
+    created () {
+      this.report_date = this.$route.query.datepicker_value  ;
+    }
+  }
 </script>
-
 <style>
 
 </style>
