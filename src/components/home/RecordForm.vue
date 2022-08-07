0bfbf95ecb218952955663920d6da9e5a9771414
tosinori-m <your.email@example.com>
2022-08-07 13:35:47 +0900

    Add record_page

--- src/components/home/RecordForm.vue
+++ src/components/home/RecordForm.vue
@@ -3,44 +3,55 @@
     <form @submit="redirectToRecord">
       <button class="ok_button">10日間の記録</button>
     </form>
+        <div v-for="record in records" :key="record.id">
+        <p>datepicker_value = {{ record.datepicker_value }}</p>
+        <p>location = {{ record.location }}</p>
+        <p>time = {{ record.time }}</p>
+        <p>condition = {{ record.condition }}</p>
+        <p>intensity = {{ record.intensity }}</p>
+        <p>point = {{ record.point }}</p>
+        <p>report = {{ record.report }}</p>
+      </div>
+    <div class="error">{{ error }}</div>
   </div>
 </template>
 <script>
-  // import axios from 'axios'
+  import axios from 'axios'
   
   export default {
-    // data () {
-    //   return {
-    //     goals: "",
-    //     error: null
-    //   }
-    // },
+    data () {
+      return {
+        records: "",
+        error: null
+      }
+    },
     methods: {
-      // async getRecord () {
-      //   try {
-      //     const res = await axios.get('http://localhost:3000/goals', {
-      //       headers: {
-      //       uid: window.localStorage.getItem('uid'),
-      //       "access-token": window.localStorage.getItem('access-token'),
-      //       client:window.localStorage.getItem('client')
-      //       }
-      //     })
-      //     if (!res) {
-      //       new Error('取得できませんでした')
-      //     }
-      //     this.goals = res.data
-      //   } catch (error) {
-      //   console.log({ error })
-      //   this.error = 'goalを表示できませんでした'
-      //   }
-      // },
+      async getRecord () {
+        try {
+          const res = await axios.get('http://localhost:3000/reports', {
+            headers: {
+            uid: window.localStorage.getItem('uid'),
+            "access-token": window.localStorage.getItem('access-token'),
+            client:window.localStorage.getItem('client')
+            }
+          })
+          if (!res) {
+            new Error('取得できませんでした')
+          }
+          this.records = res.data
+        } catch (error) {
+        console.log({ error })
+        this.error = 'recordを表示できませんでした'
+        }
+      },
       redirectToRecord () {
         this.$router.push({ name: 'RecordPage' })
+        // this.$emit("redirectTo", this.records);
       }
+    },
+    mounted() {
+      this.getRecord()
     }
-    // mounted() {
-    //   this.getRecord()
-    // }
   }
 </script>
 <style>
--- src/views/RecordPage.vue
+++ src/views/RecordPage.vue
@@ -1,8 +1,30 @@
 <template>
-  <div></div>
+  <div class = "report">
+    <!-- <h2>近日１０日間の記録</h2>
+    <RecordForm @submit="redirectToRecord" />
+    <div v-for="report in reports" :key="report.id">
+      <p>年月日 = {{ report.datepicker_value }}</p>
+    </div> -->
+  </div>
 </template>
 <script>
+//   import RecordForm from '../components/home/RecordForm.vue'
 
+// export default {
+//   components: {
+//     RecordForm,
+//   },
+//   data () {
+//     return {
+//       records: ""
+//     }
+//   },
+//   methods: {
+//     redirectToRecord(record) {
+//       this.records = record;
+//     }
+//   }
+// }
 </script>
 <style>
 
